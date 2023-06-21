8d7c21aba508099750f5339a7de60e75997fee33
ErastKoliasa <erastyk28@gmail.com>
2023-06-21 16:00:36 +0300

    added common styles for all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home/Home';
 import Play from './components/Play/Play';
 import Finish from './components/Finish/Finish';
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home/>} />
         <Route path='/play' element={<Play />} />
         <Route path='/finish' element={<Finish />} />
       </Routes>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,30 @@
 import style from './Home.module.css'
+import common from '../../common.module.css'
 const Home = () => {
     return (
-        <div className={style.home}>
-            <h1>QUIZZES</h1>
+        <div className={common.content}>
+            <h1 className={common.title}>QUIZZES</h1>
             <button className={style.luckyButton}>I`m lucky</button>
             <div className={style.quizzeContainer}>
                 <div className={style.quizzeItem}>
-                    <h3>Name</h3>
-                    <p>Number of Questions:</p>
-                    <button>Play</button>
+                    <h3 className={common.additionalTitle}>Name</h3>
+                    <p className={common.text}>Number of Questions:</p>
+                    <button className={common.button}>Play</button>
                 </div>
                 <div className={style.quizzeItem}>
-                    <h3>Name</h3>
-                    <p>Number of Questions:</p>
-                    <button>Play</button>
+                    <h3 className={common.additionalTitle}>Name</h3>
+                    <p className={common.text}>Number of Questions:</p>
+                    <button className={common.button}>Play</button>
                 </div>
                 <div className={style.quizzeItem}>
-                    <h3>Name</h3>
-                    <p>Number of Questions:</p>
-                    <button>Play</button>
+                    <h3 className={common.additionalTitle}>Name</h3>
+                    <p className={common.text}>Number of Questions:</p>
+                    <button className={common.button}>Play</button>
                 </div>
                 <div className={style.quizzeItem}>
-                    <h3>Name</h3>
-                    <p>Number of Questions:</p>
-                    <button>Play</button>
+                    <h3 className={common.additionalTitle}>Name</h3>
+                    <p className={common.text}>Number of Questions:</p>
+                    <button className={common.button}>Play</button>
                 </div>
             </div>
         </div>
