ac4806a59f290c629e0e512a22dc958451c7362c
ErastKoliasa <erastyk28@gmail.com>
2023-06-22 16:26:49 +0300

    added the functionality of displaying statistics on the "finish" page

--- src/components/Finish/Finish.jsx
+++ src/components/Finish/Finish.jsx
@@ -1,18 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import common from '../../common.module.css'
 import style from './Finish.module.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { setScore } from '../../redux/action';
 const Finish = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const questions = useSelector(state => state.questions);
+    const category = useSelector(state => state.category);
+    const score = useSelector(state => state.score)
+    const totalTime = useSelector(state => state.totalTime);
+
+    const handleClickHome = () => {
+        dispatch(setScore(0))
+        navigate("/")
+    }
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes}m ${remainingSeconds}s`;
+    };
+
     return (
         <div className={common.content}>
             <h1 className={common.title}>Quiz Results</h1>
-            <h3 className={`${common.additionalTitle} ${style.additionalTitle}`}>Name</h3>
-            <p className={`${common.text} ${style.text}`}>Total Questions: <span className={style.textDetails}>10</span></p>
-            <p className={`${common.text} ${style.text}`}>Correct Answers: <span className={style.textDetails}>10</span></p>
-            <p className={`${common.text} ${style.text}`}>Time Taken: <span className={style.textDetails}>10 seconds</span></p>
-            <p className={`${common.text} ${style.text}`}>Accuracy: <span className={style.textDetails}>100 %</span></p>
-            <p className={`${common.text} ${style.text}`}>Average Time per Question: <span className={style.textDetails}>10 seconds</span></p>
+            <h3 className={`${common.additionalTitle} ${style.additionalTitle}`}>{category}</h3>
+            <p className={`${common.text} ${style.text}`}>Total Questions: <span className={style.textDetails}>{questions.results.length}</span></p>
+            <p className={`${common.text} ${style.text}`}>Correct Answers: <span className={style.textDetails}>{score}</span></p>
+            <p className={`${common.text} ${style.text}`}>Time Taken: <span className={style.textDetails}>{formatTime(totalTime)}</span></p>
+            <p className={`${common.text} ${style.text}`}>Accuracy: <span className={style.textDetails}>{((score / questions.results.length) * 100).toFixed(2)}%</span></p>
+            <p className={`${common.text} ${style.text}`}>Average Time per Question:
+                <span className={style.textDetails}> {formatTime((totalTime / questions.results.length).toFixed(2))}</span>
+            </p>
             <div className={style.buttonHomeContainer}>
-                <Link className={style.buttonHome} to="/">HOME</Link>
+                <button className={style.buttonHome} onClick={handleClickHome}>HOME</button>
             </div>
         </div>
     )
--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -3,7 +3,7 @@ import { setCategory, setQuestions } from "./action";
 
 export const getQuestionsData = (id, name) => {
     return async (dispatch) => {
-        const res = await axios.get(`https://opentdb.com/api.php?amount=10&category=${id}`);
+        const res = await axios.get(`https://opentdb.com/api.php?amount=13&category=${id}`);
         dispatch(setQuestions(res.data));
         dispatch(setCategory(name));
     }
