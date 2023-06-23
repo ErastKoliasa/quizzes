4840dd5c6ae5d8528a0a86fb182caeaafd203ee8
ErastKoliasa <erastyk28@gmail.com>
2023-06-23 18:07:11 +0300

    change Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './components/Home/Home';
 import Play from './components/Play/Play';
 import Finish from './components/Finish/Finish';
 import Stats from './components/Stats/Stats';
 function App() {
   return (
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/play' element={<Play />} />
           <Route path='/finish' element={<Finish />} />
           <Route path='/stats' element={<Stats />} />
         </Routes>
-      </Router>
+      </HashRouter>
   );
 }
 
